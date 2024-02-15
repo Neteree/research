c90691d69e037c04fe3a930f15ee38f4e1e62f8a
Neteree <57681573+Neteree@users.noreply.github.com>
2024-02-15 20:13:53 +1300

    Update numeric_types.py

--- CodePractice/DataTypes/numeric_types.py
+++ CodePractice/DataTypes/numeric_types.py
@@ -1,1 +1,23 @@
+# Declares the variable integerA and initialises it with the value 12
+integerA = 12
+# Declares the variable integerB and initialises it with the value 43
+integerB = 43
+# Declares the variable integerC and initialises it with the value 0
+integerC = 0
+# Assigns integerC the sum of integerA and integerB
+integerC = integerA + integerB
+# Assigns integerC the difference of integerA and integerB
+integerC = integerA - integerB
+# Assigns integerC the product of integerA and integerB
+integerC = integerA * integerB
+# Assigns integerC the quotient of integerA and integerB
+integerC = integerA / integerB
+# Assigns integerC the modulo of integerA and integerB
+integerC = integerA % integerB;
 
+# Outputs the current value of integerA
+print(integerA)
+# Outputs the current value of integerB
+print(integerB)
+# Outputs the current value of integerC
+print(integerC)
