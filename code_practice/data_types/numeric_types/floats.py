c483ebad0c59ee8bd14b80caed46827a04bc3958
Neteree <57681573+Neteree@users.noreply.github.com>
2024-02-15 20:46:47 +1300

    Update floats.py

--- code_practice/data_types/numeric_types/floats.py
+++ code_practice/data_types/numeric_types/floats.py
@@ -1,1 +1,19 @@
+# Declares the float pi and initialises it with the value 3.1415
+pi = 3.1415
+# Declares the float e and initialises it with the value 2.718
+e = 2.718
+# Declares the float oneOverEight and initialises it with the quotient of 1/8
+oneOverEight = 1/8
 
+# Outputs the value of pi
+print(pi)
+# Outputs the value of e
+print(e)
+# Outputs the value of oneOverEight
+print(oneOverEight)
+# Outputs the product of 2 and pi
+print(2 * pi)
+# Outputs the product of 4 and e
+print(4 * e)
+# Outputs the product of 8 and oneOverEight
+print(8 * oneOverEight)
