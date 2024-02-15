d6cf74db0387dc300e70d1bb033fb0843c635051
Neteree <57681573+Neteree@users.noreply.github.com>
2024-02-15 21:07:41 +1300

    Update integers.py

--- code_practice/data_types/numeric_types/integers.py
+++ code_practice/data_types/numeric_types/integers.py
@@ -1,23 +1,23 @@
-# Declares the variable integerA and initialises it with the value 12
-integerA = 12
-# Declares the variable integerB and initialises it with the value 43
-integerB = 43
-# Declares the variable integerC and initialises it with the value 0
-integerC = 0
-# Assigns integerC the sum of integerA and integerB
-integerC = integerA + integerB
-# Assigns integerC the difference of integerA and integerB
-integerC = integerA - integerB
-# Assigns integerC the product of integerA and integerB
-integerC = integerA * integerB
-# Assigns integerC the quotient of integerA and integerB
-integerC = integerA / integerB
-# Assigns integerC the modulo of integerA and integerB
-integerC = integerA % integerB
+# Declares the variable integer_a and initialises it with the value 12
+integer_a = 12
+# Declares the variable integer_b and initialises it with the value 43
+integer_b = 43
+# Declares the variable integer_c and initialises it with the value 0
+integer_c = 0
+# Assigns integer_c the sum of integer_a and integer_b
+integer_c = integer_a + integer_b
+# Assigns integer_c the difference of integer_a and integer_b
+integer_c = integer_a - integer_b
+# Assigns integer_c the product of integer_a and integer_b
+integer_c = integer_a * integer_b
+# Assigns integer_c the quotient of integer_a and integer_b
+integer_c = integer_a / integer_b
+# Assigns integer_c the modulo of integer_a and integer_b
+integer_c = integer_a % integer_b
 
-# Outputs the current value of integerA
-print(integerA)
-# Outputs the current value of integerB
-print(integerB)
-# Outputs the current value of integerC
-print(integerC)
+# Outputs the current value of integer_a
+print(integer_a)
+# Outputs the current value of integer_b
+print(integer_b)
+# Outputs the current value of integer_c
+print(integer_c)
