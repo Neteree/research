d60b2d5f5431da71cf249daaa35ac2d111e15ff4
Neteree <57681573+Neteree@users.noreply.github.com>
2024-02-15 21:04:34 +1300

    Create strings.py

---
+++ code_practice/data_types/string_types/strings.py
@@ -0,0 +1,23 @@
+# Declares the variable first_name and intialises it with the string value "Cameron"
+first_name = "Cameron"
+# Declares the variable last_name and intialises it with the string value "Belcher"
+last_name = "Belcher"
+# Declares the variable full_name and intialises it with first_name + " " + last_name
+full_name = first_name + " " + last_name
+# Declares the variable food and intialises it with the string value "pizza"
+food = "pizza"
+# Assigns food with a new string value "mince and cheese pie"
+food = "mince and cheese pie"
+# Assigns food with a new string value "cheeseburger"
+food = "cheeseburger"
+# Declares the variable drink and intialises it with the string value "coke"
+drink = "coke"
+# Declares the variable side and intialises it with the string value "fries"
+side = "fries"
+# Declares the variable combo and intialises with the string value food + ", " + drink + " and " + side
+combo = food + ", " + drink + " and " + side
+
+# Outputs the value of full_name
+print(full_name)
+# Outputs the value of combo
+print(combo)
