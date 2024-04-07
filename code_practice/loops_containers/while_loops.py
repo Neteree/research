8fac4575927e317debd075b69818b4e2981dbd77
Neteree <57681573+Neteree@users.noreply.github.com>
2024-04-07 14:40:24 +1200

    Create while_loops.py

---
+++ code_practice/loops_containers/while_loops.py
@@ -0,0 +1,31 @@
+# Declares the variable count and initialises it with the value 0
+count = 0
+#Loops while count is less than 10
+while(count < 10):
+    #Increments count by 1 for each iteraction of the loop
+    count += 1
+    #Prints count for each iteration of the loop
+    print(count)
+    
+#Loops while count is greater than 1
+while(count > 1):
+    #Decrements count by 1 for each iteraction of the loop
+    count -= 1
+    #Prints count for each iteration of the loop
+    print(count)
+    
+#Asigns 0 to count  
+count = 0
+#Loops while count is less than 50
+while(count < 50):
+    #Increments count by 5 for each iteraction of the loop
+    count += 5
+    #Prints count for each iteration of the loop
+    print(count)
+    
+#Loops while count is greater than 5
+while(count > 5):
+    #Decrements count by 5 for each iteraction of the loop
+    count -= 5
+    #Prints count for each iteration of the loop
+    print(count)
